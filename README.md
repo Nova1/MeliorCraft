d308389aebc3799471fb597d9e82799ad9b0b19b
Nova1 <mckibbles2010@gmail.com>
2014-05-06 17:38:00 +0100

    Update
    Update

--- README.md
+++
@@ -1,4 +0,0 @@
-MeliorCraft
-===========
-
-Minecraft mod
--- src/main/java/com/melior/minecraft/MeliorCraft.java
+++ src/main/java/com/melior/minecraft/MeliorCraft.java
@@ -13,10 +13,8 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 
-@Mod(modid = MeliorCraft.MODID, version = MeliorCraft.VERSION)
+@Mod(modid = "meliorcraft")
 public class MeliorCraft {
-	public static final String MODID = "meliorcraft";
-	public static final String VERSION = "1.0";
 //Main Section of code
 
 	
